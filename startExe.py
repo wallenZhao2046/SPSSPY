f1892060562ec5937c64a195f5f0fc6278fc7174
wallenZhao2046 <wazhao@qq.com>
2016-08-05 18:21:02 +0800

    updated

--- startExe.py
+++ startExe.py
@@ -1,22 +1,7 @@
 import sys
 import os
 import executeSpssScript as scr
-from bs4 import BeautifulSoup
-
-
-html = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-<p class="story">...</p>
-"""
-
-soup = BeautifulSoup(html)
+# from bs4 import BeautifulSoup
 
 
 # default input data dir is ./data
